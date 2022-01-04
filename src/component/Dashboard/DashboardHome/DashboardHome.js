23ab395b6703dd7a14f93bb731955637eff82713
Chayti Saha <chaytisaha98@gmail.com>
2022-01-04 14:54:02 +0600

    profile done

--- src/component/Dashboard/DashboardHome/DashboardHome.js
+++ src/component/Dashboard/DashboardHome/DashboardHome.js
@@ -1,9 +1,26 @@
 import * as React from 'react';
+import useAuth from '../../../hooks/useAuth';
+import './DashboardHome.css';
 
 const DashboardHome = () => {
+    const { user } = useAuth();
     return (
-        <h1 className="admin-banner">Welcome to your Dashboard</h1>
-
+        <div className="dashboard-banner dashboard-front-bg">
+            <h1 className="fw-bold mb-5">Your Profile</h1>
+            <div className="front-bg-profile">
+                {
+                    user.email && <img width="150" style={{ borderRadius: "50%", border: "3px solid #331a15", color: "white" }} src={user.photoURL} alt="" />
+                }
+                <div className="py-3">
+                    {
+                        user.displayName && <h4 className="fw-bold" style={{ fontFamily: 'Rancho' }}>Name: {user.displayName}</h4>
+                    }
+                    {
+                        user.email && <h5>Email: {user.email}</h5>
+                    }
+                </div>
+            </div>
+        </div>
     );
 };
 
